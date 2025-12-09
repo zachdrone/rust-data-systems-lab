10bbc711ed0e8b98af8de71cba1ad4669c4e4b61
Zach Drone <zvdrone@gmail.com>
2025-12-08 23:13:27 -0500

    perf: use spawn blocking

--- crates/etl-engine/src/bin/csv_to_arrow.rs
+++ crates/etl-engine/src/bin/csv_to_arrow.rs
@@ -7,36 +7,39 @@ use std::fs::File;
 use std::sync::Arc;
 use tokio::sync::mpsc;
 
-async fn read_csv(
-    filepath: &str,
-    schema: Arc<arrow_schema::Schema>,
-    tx: mpsc::Sender<RecordBatch>,
-) {
-    let file = File::open(filepath).unwrap();
+async fn read_csv(filepath: String, schema: Arc<Schema>, tx: mpsc::Sender<RecordBatch>) {
+    tokio::task::spawn_blocking(move || {
+        let file = File::open(filepath).unwrap();
 
-    let mut csv = ReaderBuilder::new(schema)
-        .with_header(true)
-        .build(file)
-        .unwrap();
-    while let Some(batch) = csv.next() {
-        tx.send(batch.unwrap()).await.unwrap();
-    }
+        let csv = ReaderBuilder::new(schema)
+            .with_header(true)
+            .build(file)
+            .unwrap();
 
-    drop(tx);
+        for batch in csv {
+            tx.blocking_send(batch.unwrap()).unwrap();
+        }
+    })
+    .await
+    .unwrap();
 }
 
 async fn normalize(tx: mpsc::Sender<RecordBatch>, mut rx: mpsc::Receiver<RecordBatch>) {
     while let Some(batch) = rx.recv().await {
-        let idx = batch.schema().index_of("heart_rate_bpm").unwrap();
-        let col = batch
-            .column(idx)
-            .as_any()
-            .downcast_ref::<Int64Array>()
-            .unwrap();
+        let tx = tx.clone();
 
-        let normed_col = normalize_i64(&col);
-        let normed_batch = add_normalized_col(&batch, normed_col, "normed_heart_rate_bpm");
-        tx.send(normed_batch).await.unwrap();
+        tokio::task::spawn_blocking(move || {
+            let idx = batch.schema().index_of("heart_rate_bpm").unwrap();
+            let col = batch
+                .column(idx)
+                .as_any()
+                .downcast_ref::<Int64Array>()
+                .unwrap();
+
+            let normed_col = normalize_i64(&col);
+            let normed_batch = add_normalized_col(&batch, normed_col, "normed_heart_rate_bpm");
+            tx.blocking_send(normed_batch).unwrap();
+        });
     }
 }
 
@@ -60,7 +63,11 @@ async fn main() {
         Field::new("pace_min_per_mile", DataType::Float32, false),
     ]));
 
-    let reader = tokio::spawn(read_csv("running_data.csv", schema.clone(), raw_tx));
+    let reader = tokio::spawn(read_csv(
+        "running_data.csv".to_owned(),
+        schema.clone(),
+        raw_tx,
+    ));
     let normalizer = tokio::spawn(normalize(norm_tx, raw_rx));
     let writer = tokio::spawn(write_output(norm_rx));
 
